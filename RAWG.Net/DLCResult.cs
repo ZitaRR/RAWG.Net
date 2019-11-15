05cb2bcbfa55f6201b81811d814808038e54fb50
ZitaRR <haeggqvist1998@hotmail.com>
2019-11-16 00:32:36 +0100

    Created DLCResult

---
+++ RAWG.Net/DLCResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RAWG.Net
+{
+    public class DLCResult : Result
+    {
+        public string Name { get; private set; }
+        public int? ID { get; private set; }
+        public DateTime? Released { get; private set; }
+
+        private int count;
+        private dynamic data;
+
+        public DLCResult(int count = 0, int index = 0, params dynamic[] results)
+        {
+            if (count <= 0)
+                return;
+
+            this.count = count;
+            data = results;
+
+            Name = data[index].name;
+            ID = data[index].id;
+            Released = DateTime.TryParse(data[index].released.ToString(), out DateTime time) ? time as DateTime? : null;
+        }
+
+        internal DLCResult[] Initialize()
+        {
+            if (count <= 0)
+                return null;
+
+            var dlcs = new DLCResult[count];
+            for (int i = 0; i < count; i++)
+                dlcs[i] = new DLCResult(count, i, data);
+            return dlcs;
+        }
+
+        public override string ToString()
+        {
+            string release = GetValue(Released);
+
+            return $"Title: {Name}\n" +
+                $"ID: {ID}\n" +
+                $"Released: {release}\n\n";
+        }
+    }
+}
